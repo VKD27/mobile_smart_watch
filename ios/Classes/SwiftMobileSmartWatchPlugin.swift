b9d786f05ea2fd60b55a3421f336166df6dd9887
Vivek <vickyvivek2727@gmail.com>
2022-07-05 10:45:23 +0530

    check test for the ble devices return

--- ios/Classes/SwiftMobileSmartWatchPlugin.swift
+++ ios/Classes/SwiftMobileSmartWatchPlugin.swift
@@ -2,7 +2,7 @@ import Flutter
 import UIKit
 import UTESmartBandApi
 
-public class SwiftMobileSmartWatchPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, UTEManagerDelegate{
+class SwiftMobileSmartWatchPlugin: NSObject, FlutterPlugin, FlutterStreamHandler, UTEManagerDelegate{
     
     private var eventSink: FlutterEventSink?
     //private var callbackId :NSMutableDictionary
@@ -11,9 +11,9 @@ public class SwiftMobileSmartWatchPlugin: NSObject, FlutterPlugin, FlutterStream
     public typealias findDevicesBlock = () -> Void
     
     var smartBandMgr = UTESmartBandClient.init()
-   //var smartBandTool  = SmartBandDelegateTool.init()
+    //var smartBandTool  = SmartBandDelegateTool.init()
     
-   // weak var uteManagerDelegate : UTEManagerDelegate?
+    // weak var uteManagerDelegate : UTEManagerDelegate?
     
     //weak var connectVc : SmartBandConnectedControl?
     
@@ -48,24 +48,112 @@ public class SwiftMobileSmartWatchPlugin: NSObject, FlutterPlugin, FlutterStream
         //
         //        }
         
-//        self.uteManagerDelegate?.uteManagerDiscover?(T##modelDevices: UTEModelDevices!##UTEModelDevices?) { modelDevices in
-//
-//        }
-       
+        //        self.uteManagerDelegate?.uteManagerDiscover?(T##modelDevices: UTEModelDevices!##UTEModelDevices?) { modelDevices in
+        //
+        //        }
+        
     }
     
     // MARK: - UTEManagerDelegate
-    public func uteManagerDiscover(_ modelDevices: UTEModelDevices!) {
-        print(modelDevices!)
+    func uteManagerDiscover(_ modelDevices: UTEModelDevices!) {
+        
+        var sameDevices = false
+        for model in self.mArrayDevices {
+            if (model.identifier?.isEqual(modelDevices.identifier as String))! {
+                model.rssi = modelDevices.rssi
+                model.name = modelDevices.name
+                sameDevices = true
+                break
+            }
+            
+        }
+        
+        if !sameDevices {
+            print("***Scanned device name=\(String(describing: modelDevices.name)) id=\(String(describing: modelDevices.identifier))")
+            self.mArrayDevices.append(modelDevices)
+        }
+        
+        //print(modelDevices!)
         print("in discover is \(modelDevices!)")
         print("Inside the discover callback ", modelDevices!)
         print("****** data = \(String(describing: modelDevices))")
     }
     
-    public func uteManagerDevicesSate(_ devicesState: UTEDevicesSate, error: Error!, userInfo info: [AnyHashable : Any]! = [:]) {
+    func uteManagerDevicesSate(_ devicesState: UTEDevicesSate, error: Error!, userInfo info: [AnyHashable : Any]! = [:]) {
         print("****** devicesState = \(String(describing: devicesState))")
         print("****** error = \(String(describing: error))")
         print("****** userInfo = \(String(describing: info))")
+    }
+    
+    func uteManagerExtraIsAble(_ isAble: Bool) {
+        if isAble {
+            print("***Successfully turn on the additional functions of the device")
+        }else{
+            print("***Failed to open the extra functions of the device, the device is actively disconnected, please reconnect the device")
+        }
+    }
+    func uteManagerReceiveTodaySteps(_ runData: UTEModelRunData!) {
+        print("***Today time=\(String(describing: runData.time))，Total steps=\(runData.totalSteps),Total distance=\(runData.distances),Total calories=\(runData.calories),Current hour steps=\(runData.hourSteps)")
+    }
+    
+    func uteManagerReceiveTodaySport(_ dict: [AnyHashable : Any]!) {
+        let walk : UTEModelSportWalkRun = dict[kUTEQuerySportWalkRunData] as! UTEModelSportWalkRun
+        print("sport device step=\(walk.stepsTotal)")
+    }
+    func uteManagerUTEIbeaconOption(_ option: UTEIbeaconOption, value: String!) {
+        print("ibeacon value = \(String(describing: value))")
+    }
+    
+    func uteManagerTakePicture() {
+        print("***I took a photo, if I don’t take a photo, please exit the photo mode")
+    }
+    
+    func uteManagerBluetoothState(_ bluetoothState: UTEBluetoothState) {
+        
+        print("****** bluetoothState = \(String(describing: bluetoothState))")
+        
+        if bluetoothState == UTEBluetoothState.close {
+           // if self.alertView != nil {
+           //     return
+           // }
+           // weak var weakSelf = self
+            DispatchQueue.main.async {
+                //Please turn on the phone Bluetooth
+                //let alterVC = UIAlertController.init(title: "hint", message: "Please turn on your phone's bluetooth", preferredStyle: UIAlertController.Style.alert)
+                
+                //weakSelf?.alertView = alterVC
+                
+                //let window = UIApplication.shared.keyWindow
+               // let nav : UINavigationController = window?.rootViewController as! UINavigationController
+                
+               // alterVC.addAction(UIAlertAction.init(title: "it is good", style: UIAlertAction.Style.cancel, handler: { (cancelAction) in
+                    
+              //  }))
+                
+               // nav.present(alterVC, animated: true, completion: nil)
+                
+            }
+        }else{
+            //self.alertView?.dismiss(animated: true, completion: nil)
+           // self.alertView = nil
+        }
+        
+    }
+    
+    func uteManagerReceiveCustomData(_ data: Data!, result: Bool) {
+        if result {
+            print("******Successfully received data = \(String(describing: data))")
+        }else{
+            print("***Failed to receive data")
+        }
+    }
+    
+    func uteManagerSendCustomDataResult(_ result: Bool) {
+        if result {
+            print("***Send custom data successfully")
+        }else{
+            print("***Failed to send custom data")
+        }
     }
     
     public static func register(with registrar: FlutterPluginRegistrar) {
@@ -101,7 +189,7 @@ public class SwiftMobileSmartWatchPlugin: NSObject, FlutterPlugin, FlutterStream
         switch call.method {
         case GlobalConstants.DEVICE_RE_INITIATE:
             self.deviceReInitialize(returnResult: result)
-        
+            
         case GlobalConstants.DEVICE_INITIALIZE:
             self.deviceInitialize(returnResult: result)
             
@@ -157,33 +245,58 @@ public class SwiftMobileSmartWatchPlugin: NSObject, FlutterPlugin, FlutterStream
     }
     
     public func deviceInitialize(returnResult: FlutterResult){
-        self.smartBandMgr.initUTESmartBandClient()
-       
-        self.smartBandMgr.debugUTELog = true
-       
-        self.smartBandMgr.isScanRepeat = true
         
-        self.smartBandMgr.filerRSSI = -60
         
-        self.smartBandMgr.filerServers = ["5533","2222","FEE7"]
-        
-        print("log sdk vsersion = \(self.smartBandMgr.sdkVersion())")
+        //print("log sdk vsersion = \(self.smartBandMgr.sdkVersion())")
         // return nil
         //self.smartBandMgr.startScanDevices()
         //self.smartBandMgr.stopScanDevices()
         
         //self.smartBandMgr.delegate = self.smartBandTool
-        returnResult(GlobalConstants.SC_INIT)
+        //returnResult(GlobalConstants.SC_INIT)
+        
+        //do {
+            self.smartBandMgr.initUTESmartBandClient()
+            
+            self.smartBandMgr.debugUTELog = true
+            
+            self.smartBandMgr.isScanRepeat = true
+            
+            self.smartBandMgr.filerRSSI = -60
+            
+            
+            self.smartBandMgr.filerServers = ["5533","2222","FEE7"]
+            
+            print("log sdk vsersion = \(self.smartBandMgr.sdkVersion())")
+            
+            //                if let bundlePath = Bundle.main.path(forResource: name,
+            //                                                     ofType: "json"),
+            //                    let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+            //                    return jsonData
+            //                }
+            returnResult(GlobalConstants.SC_INIT)
+        //} catch {
+       //     print("IOS: Could not initalize")
+       //     returnResult(GlobalConstants.SC_FAILURE)
+       // }
     }
     
     public func searchForBTDevices(result: FlutterResult){
         print("inseide device start scan")
-
+        
         DispatchQueue.main.async {
             self.smartBandMgr.startScanDevices()
         }
         
-        result(GlobalConstants.SC_INIT)
+        //let jsonObject = createJSONObject(firstName: "firstName", middleName: "middleName", lastName: "lastName", age: 21, weight: 82)
+        let jsonObject: [String: Any] = [
+            "status" : GlobalConstants.SC_SUCCESS,
+            "data": []
+        ]
+        
+        let resultData = try! JSONSerialization.data(withJSONObject: jsonObject)
+        let jsonString = String(data: resultData, encoding: .utf8)!
+        result(jsonString)
     }
     
     private func getCheckConnectionStatus(result: FlutterResult) {
